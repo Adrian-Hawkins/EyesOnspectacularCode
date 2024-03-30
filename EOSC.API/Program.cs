c8ac24d0daee27bdddde26e88734599bde9f1096
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-03-30 17:56:46 +0200

    Added 'facade' auth to swagger need to actually implement it

--- EOSC.API/Program.cs
+++ EOSC.API/Program.cs
@@ -1,4 +1,5 @@
 using EOSC.API.Service;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // TODO: Add support for Github Tokens
+    var openApiSecurityScheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        },
+        Type = SecuritySchemeType.Http,
+        Scheme = "oauth2",
+        Name = "Bearer",
+        In = ParameterLocation.Header
+    };
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme());
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { openApiSecurityScheme, new List<string>() }
+    });
+});
 
 builder.Services.AddSingleton<IBase64Service, Base64Service>();
 
