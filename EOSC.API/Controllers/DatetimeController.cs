cb8a97aa4ecd1d584450ef4a732f827bca4012d3
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-04-04 22:34:58 +0200

    feat: :sparkles: Added date time endpoint

---
+++ EOSC.API/Controllers/DatetimeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EOSC.Common.Requests;
+using EOSC.Common.Responses;
+
+namespace EOSC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatetimeController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Post([FromBody] DatetimeRequest request)
+        {
+            DateTime dateTime;
+            if (!DateTime.TryParseExact(request.dateTimeString, request.originalFormat, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dateTime))
+            {
+                return BadRequest($"Invalid date-time string: {request.dateTimeString}");
+            }
+            string convertedDateTime = dateTime.ToString(request.desiredFormat, System.Globalization.CultureInfo.InvariantCulture);
+            var response = new DateTimeConversionResponse(convertedDateTime);
+            return Ok(response);
+        }
+    }
+}
