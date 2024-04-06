de589a66fd4e8c66d49dc7e3202e03db7405f6d7
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-04-06 18:25:18 +0200

    refactor: :package: Removed discord.net

--- EOSC.Bot/Interfaces/Classes/IDiscordBot.cs
+++ EOSC.Bot/Interfaces/Classes/IDiscordBot.cs
@@ -1,11 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace EOSC.Bot.Interfaces.Classes
+﻿namespace EOSC.Bot.Interfaces.Classes
 {
     public interface IDiscordBot
     {
