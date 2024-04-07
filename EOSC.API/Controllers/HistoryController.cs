40d058aaf621d934610130000be33c02c5175d0e
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-04-07 19:54:36 +0200

    feat: :sparkles: Database history

---
+++ EOSC.API/Controllers/HistoryController.cs
@@ -0,0 +1,24 @@
+﻿using EOSC.API.Repo;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EOSC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+        [HttpGet("{username}")]
+        public IActionResult Get(string username)
+        {
+            List<string> history = HistoryRepo.GetHistory(username);
+            if(history.Count == 0 || history == null)
+            {
+                return BadRequest($"Invalid username: {username}");
+            }
+            return Ok(history);
+        }
+
+    }
+}
---
+++ EOSC.API/Repo/Connection.cs
@@ -0,0 +1,41 @@
+﻿using EOSC.API.Config;
+using Npgsql;
+
+namespace EOSC.API.Repo
+{
+    public class Connection
+    {
+        private static string ConnectionString;
+        private static NpgsqlConnection _connection;
+
+        private Connection() {}
+
+        public static NpgsqlConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                IConfiguration config = new ConfigurationBuilder()
+                 .AddUserSecrets<DB>()
+                 .AddEnvironmentVariables()
+                 .Build();
+                ConnectionString = config["database:connection"] ?? throw new Exception("Please provide database connection string");
+                _connection = new NpgsqlConnection(ConnectionString);
+                _connection.Open();
+            }
+            else if (_connection.State != System.Data.ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+
+            return _connection;
+        }
+
+        public static void CloseConnection()
+        {
+            if (_connection != null && _connection.State != System.Data.ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
