716ef0c3cd96ee424ef0145095033cde094b5814
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-04-09 22:16:34 +0200

    feat: :sparkles: Json to yaml

--- EOSC.API/Controllers/JsonToYamlController.cs
+++ EOSC.API/Controllers/JsonToYamlController.cs
@@ -4,18 +4,20 @@ using EOSC.Common.Responses;
 using EOSC.Common.Services.ChatGPT;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace EOSC.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class YamlToJsonController : ControllerBase
+    public class JsonToYamlController : ControllerBase
     {
-        [Tool("YamlToJson")]
+        [Tool("JsonToYaml")]
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] YamlToJsonRequest request)
         {
             string query = $@"
-                    Please convert this yaml to json, in your response only provide the converted json, only say the word ""invalid"" if the yaml found is not valid
+                    Please convert this json to yaml, in your response only provide the converted yaml, only say the word ""invalid"" if the json found is not valid
 
                     {request.data}
             ";
---
+++ EOSC.Bot/Commands/JsonToYaml.cs
@@ -0,0 +1,36 @@
+﻿using EOSC.Bot.Attributes;
+using EOSC.Bot.Classes.Deserializers;
+using EOSC.Common.Requests;
+using EOSC.Common.Responses;
+using EOSC.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOSC.Bot.Commands
+{
+    [Command("JsonToYaml")]
+    public class JsonToYaml : BaseCommand
+    {
+        private readonly ApiCallService _apiCallService = new();
+        public override async Task SendCommand(string discordToken, List<string> args, Message message)
+        {
+            string json = string.Join(" ", args).Replace("\"", "'");
+
+            var request = new YamlToJsonRequest
+            (
+                json
+            );
+
+            var response =
+            await _apiCallService.MakeApiCall<YamlToJsonRequest, YamlToJsonResponse>(
+                "/api/JsonToYaml",
+                request
+            );
+
+            await SendMessageAsync($"{response.result}", message.ChannelId, discordToken);
+        }
+    }
+}
