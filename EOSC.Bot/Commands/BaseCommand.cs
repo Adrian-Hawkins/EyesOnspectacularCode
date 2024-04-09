eaba6501449c94a0dacc0f406a3ed0193f5e1714
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-04-09 21:02:01 +0200

    Fixed import

--- EOSC.Bot/Commands/BaseCommand.cs
+++ EOSC.Bot/Commands/BaseCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using EOSC.Bot.Attributes;
 using EOSC.Bot.Classes.Deserializers;
 
@@ -12,70 +13,39 @@ public abstract class BaseCommand
         return attribute?.CommandName;
     }
 
-        public abstract Task SendCommand(string botToken, List<string> args, Message message);
+    public abstract Task SendCommand(string botToken, List<string> args, Message message);
 
 
-        public record Resp
+    public record Resp
+    {
+        [JsonPropertyName("content")] public required string Content { get; set; }
+    }
+
+
+    protected async Task SendMessageAsync(string message, string channelId, string botToken)
+    {
+        try
         {
-            [JsonPropertyName("content")] public required string Content { get; set; }
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bot {botToken}");
+
+            var url = $"https://discordapp.com/api/v9/channels/{channelId}/messages";
+            var req = new Resp
+            {
+                Content = message
+            };
+
+            var jsonContent = JsonContent.Create(req, typeof(Resp));
+            Console.WriteLine(await jsonContent.ReadAsStringAsync());
+
+            var responseMessage = await httpClient.PostAsJsonAsync(url, req);
+
+            var readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
+            Console.WriteLine(readAsStringAsync);
         }
-
-
-        protected async Task SendMessageAsync(string message, string channelId, string botToken)
+        catch (Exception ex)
         {
-            try
-            {
-                using var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bot {botToken}");
-
-                var url = $"https://discordapp.com/api/v9/channels/{channelId}/messages";
-                var req = new Resp
-                {
-                    Content = message
-                };
-
-                var jsonContent = JsonContent.Create(req, typeof(Resp));
-                Console.WriteLine(await jsonContent.ReadAsStringAsync());
-
-                var responseMessage = await httpClient.PostAsJsonAsync(url, req);
-
-                var readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
-                Console.WriteLine(readAsStringAsync);
-
-                // responseMessage.EnsureSuccessStatusCode();
-
-
-                /*string url = $"https://discordapp.com/api/v9/channels/{channelId}/messages";
-                string jsonPayload = $"{{\"content\": \"{message}\"}}";
-                var resp = new Resp
-                {
-                    Content = message
-                };
-                using var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bot {botToken}");
-                var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-
-                try
-                {
-                    HttpResponseMessage response = await httpClient.PostAsync(url, content);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine("POST request successful. Message sent.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"POST request failed with status code: {response.StatusCode}");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                }*/
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error processing message: {ex.Message}");
-            }
+            Console.WriteLine($"Error processing message: {ex.Message}");
         }
     }
 }
